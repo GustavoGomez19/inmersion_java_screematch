ad4b626302514738c1d895e4718a0d20f84dc7c7
GGomezQ <gagomezq1992@gmail.com>
2024-05-03 13:11:24 -0500

    Avance de optimización

--- src/modelo/Pelicula.java
+++ src/modelo/Pelicula.java
@@ -1,9 +1,13 @@
 package modelo;
+import java.util.Scanner;
 
 public class Pelicula extends  Titulo{
+    Scanner entrada = new Scanner(System.in);
 
+    //Atributo propio de la clase
     private String director;
 
+    //Métodos Getter y Setter
     public String getDirector() {
         return director;
     }
@@ -11,4 +15,20 @@ public class Pelicula extends  Titulo{
     public void setDirector(String director) {
         this.director = director;
     }
+
+    //Método sobrescrito de la clase Titulo para ingresar la información específica de la pelícuala
+    @Override
+    public void ingresarInformacionTitulo() {
+        super.ingresarInformacionTitulo();
+        System.out.print("Ingrese el nombre del director: ");
+        setDirector(entrada.nextLine());
+    }
+
+    //Método sobrescrito de la clase Titulo para mostrar la información específica de la película
+    @Override
+    public void verInfoTitulo() {
+        super.verInfoTitulo();
+        System.out.println("Director: " + director);
+    }
+
 }
